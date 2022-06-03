dbf660f9f39355f8a9b2a36eea164d76e80b277f
Andrey <kietrivers99@gmail.com>
2022-06-03 11:09:45 +0300

    Task4

--- src/Day5/Day5Task1.java
+++ src/Day5/Day5Task1.java
@@ -1,4 +1,51 @@
 package Day5;
+import java.util.Scanner;
 
 public class Day5Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Car car1 = new Car();
+        car1.setColor("blue");
+        car1.setModel("jaguar");
+        car1.setYear(1993);
+        System.out.println("Color: " + car1.getColor());
+        System.out.println("Model: " + car1.getModel());
+        System.out.println("Year of issue: " + car1.getYear());
+        car1.Char();
+
+    }
 }
+
+class Car {
+    private String model;
+    private int year;
+    private String color;
+
+    public void setModel(String userModel) {
+        model = userModel;
+    }
+    public void setYear(int userYear) {
+        year = userYear;
+    }
+    public void setColor(String userColor) {
+        color = userColor;
+    }
+    public String getModel() {
+        return model;
+    }
+    public int getYear() {
+        return year;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void Char() {
+        System.out.println("Color: " + color);
+        System.out.println("Model: " + model);
+        System.out.println("Year : " + year);
+
+    }
+
+}
+
+
---
+++ src/Day5/Day5Task2.java
@@ -0,0 +1,4 @@
+package Day5;
+
+public class Day5Task2 {
+}
