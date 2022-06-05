40c87564a0769a855321dce308ca958efa3d9d42
Andrey <kietrivers99@gmail.com>
2022-06-05 17:36:21 +0300

    Task4

---
+++ src/Day6/Task3.java
@@ -0,0 +1,67 @@
+package Day6;
+import  java.util.Random;
+public class Task3 {
+    public static void main(String[] args){
+        Teacher teacher = new Teacher("Math", "Teacher");
+        Student student = new Student("Student");
+        Random random = new Random();
+        teacher.evaluate(student);
+
+    }
+}
+
+
+
+
+class Student {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+}
+class Teacher{
+    private String name;
+    private String lesson;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLesson() {
+        return lesson;
+    }
+
+    public Teacher(String lesson, String name) {
+        this.lesson = lesson;
+        this.name = name;
+    }
+    public void evaluate (Student student) {
+        Random random = new Random();
+        int randomGrade = random.nextInt(5) + 2;
+        String grade = "";
+        switch(randomGrade) {
+            case 2:
+                grade = "F";
+                break;
+            case 3:
+                grade = "C/D";
+                break;
+            case 4:
+                grade = "B";
+                break;
+            case 5:
+                grade = "A";
+                break;
+        }
+        System.out.println("Teacher " + this.name + " graded : " + student.getName() + " in the subject: " + this.lesson +
+                " with grade: " + grade);
+
+
+    }
+
+}
